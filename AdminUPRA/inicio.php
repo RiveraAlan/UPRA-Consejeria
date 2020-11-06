e688f7c92381a235f4e085684ab7caacb3b14db4
octaviani21 <nelson.octaviani@upr.edu>
2020-11-05 23:23:51 -0400

    cambios

--- AdminUPRA/inicio.php
+++ AdminUPRA/inicio.php
@@ -37,38 +37,6 @@ if(!isset($_SESSION['id'])){
         <a href="index.html" class="nav-link">Inicio</a>
       </li>
     </ul>
-
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      <!-- Notifications Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-bell"></i>
-          <span class="badge badge-danger navbar-badge">15</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-item dropdown-header">15 Notifications</span>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i> 4 new messages
-            <span class="float-right text-muted text-sm">3 mins</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i> 8 friend requests
-            <span class="float-right text-muted text-sm">12 hours</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-file mr-2"></i> 3 new reports
-            <span class="float-right text-muted text-sm">2 days</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-        </div>
-      </li>
-      
-    </ul>
   </nav>
   <!-- /.navbar -->
 
@@ -303,10 +271,7 @@ if(!isset($_SESSION['id'])){
                           <span class='badge badge-success'>SI</span>
                       </td>
                       <td class='project-actions text-right'>
-                          <a class='btn btn-primary btn-sm' href='editest.html'>
-                              <i class='fas fa-user-edit'></i>
-                              Editar
-                          </a>
+                          
                           <div style='padding-top: 10px;'>
                           <a class='btn btn-danger btn-sm' href='#''>
                              <i class='fas fa-user-times'></i>
