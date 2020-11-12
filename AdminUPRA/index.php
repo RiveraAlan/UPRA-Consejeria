c6fe6e6bcb8591afa3afc2e4a84d11baad99bb94
octaviani21 <nelson.octaviani@upr.edu>
2020-11-12 19:36:57 -0400

    Update index.php

--- AdminUPRA/index.php
+++ AdminUPRA/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="login.css">
-    <title>CONSEJERIA-UPRA | LOGIN</title>
+    <title>CONSEJERÍA-UPRA | INICIO SESIÓN</title>
 </head>
 <body>
      <div class="background-dark">
@@ -13,10 +13,10 @@
             <h1 class="title">Consejería UPRA</h1>
             <?php 
             if(isset($_GET['isEmailEmpty']) || isset($_GET['isPasswordEmpty'])){
-                echo '<div class="error-message">Please fill both the username and password fields!</div>';
+                echo '<div class="error-message">¡Por favor, complete los campos de nombre de usuario y contraseña!</div>';
             } 
             if(isset($_GET['isAuthFailed'])){
-                echo '<div class="error-message">Incorrect username and/or password!</div>';
+                echo '<div class="error-message">¡Nombre de usuario y / o contraseña incorrectos!</div>';
             }
             ?>
          </div>
@@ -25,14 +25,14 @@
 
         <form class="login-form" action="inc/admin_auth.php" method="POST">
              <div class="form-group">
-                 <div class="<?= isset($_GET['isEmailEmpty']) && $_GET['isEmailEmpty'] ? 'form-group-label-invalid' : 'form-group-label'?>">E-mail</div>
+                 <div class="<?= isset($_GET['isEmailEmpty']) && $_GET['isEmailEmpty'] ? 'form-group-label-invalid' : 'form-group-label'?>">Correo Electrónico</div>
                  <input type="email" name="email" id="" class="<?= isset($_GET['isEmailEmpty']) && $_GET['isEmailEmpty'] ? 'form-input-invalid' : 'form-input'?>">
              </div>
              <div class="form-group">
-                 <div class="<?= isset($_GET['isPasswordEmpty']) && $_GET['isPasswordEmpty'] ? 'form-group-label-invalid' : 'form-group-label'?>">Password</div>
+                 <div class="<?= isset($_GET['isPasswordEmpty']) && $_GET['isPasswordEmpty'] ? 'form-group-label-invalid' : 'form-group-label'?>">Contraseña</div>
                  <input type="password" name="password" id="" class="<?= isset($_GET['isPasswordEmpty']) && $_GET['isPasswordEmpty'] ? 'form-input-invalid' : 'form-input'?>">
              </div>
-             <div class="mx-auto"><button type="submit" class="submit-btn">Login</button></div>
+             <div class="mx-auto"><button type="submit" class="submit-btn">Iniciar Sesión</button></div>
         </form>
 
 
