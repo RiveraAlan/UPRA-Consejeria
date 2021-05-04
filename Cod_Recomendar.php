b6941ab8e78446381a719c1c21d18b3cd1a228b8
LizMatos <liz.matos2@upr.edu>
2021-05-03 20:42:39 -0400

    Update Cod_Recomendar.php

--- Cod_Recomendar.php
+++ Cod_Recomendar.php
@@ -1,18 +1,88 @@
 <?php
+session_start();
+include("inc/connection.php");
 
- $sql_SA =  "SELECT crse_code, crse_year, crse_semester 
+if(isset($_GET["stdnt_number"])){
+  $student_id = $_GET['stdnt_number'];
+} else {
+  $student_id = $_SESSION['stdnt_number'];
+}
+$advisor_id = $_SESSION['adv_email'];
+
+if(!isset($student_id)){
+  header("Location: index.php");
+    exit();
+}
+
+$query = "SELECT * FROM stdnt_record WHERE  stdnt_number = '$_SESSION[stdnt_number]'";
+$result = mysqli_query($conn, $query);
+$resultCheck = mysqli_num_rows($result);
+$isRecordPresentInDB = FALSE;
+
+if($resultCheck > 0)
+  $isRecordPresentInDB = TRUE;
+
+    $modal = 'document.getElementById("id03").style.display="block"';
+?>
+
+<?php
+
+$mes = date("m");
+if ($mes<7)
+$semestre = 2;
+else 
+$semestre = 1;
+ 
+$sql = "SELECT stdnt_number
+        FROM student
+        WHERE stdnt_number = '$student_id'";
+                  $result = mysqli_query($conn, $sql);
+                  $resultCheck = mysqli_num_rows($result);
+                  $row = mysqli_fetch_assoc($result);
+$est_year = date('Y')-(substr($row['stdnt_number'], 4,2) + 1999);
+
+$sql_SA =  "SELECT crse_code, crse_year, crse_semester 
             FROM cohort
             WHERE crse_major = 'CC COMS BCN'";
                       $result_SA = mysqli_query($conn, $sql_SA);
                       $resultCheck_SA = mysqli_num_rows($result_SA);
-                      $row_SA = mysqli_fetch_assoc($result_SA);
 
- $sql_PC =  "SELECT crse_code, crse_PRE, crse_CO
+  if($resultCheck_SA > 0){
+  while($row_SA = mysqli_fetch_assoc($result_SA)){
+$sql_P =  "SELECT crse_code, crse_PRE
             FROM cohort INNER JOIN scheme USING (crse_code)
-            WHERE crse_major = 'CC COMS BCN' AND crse_code = 'CCOM4007'";
-                      $result_PC = mysqli_query($conn, $sql_PC);
-                      $resultCheck_PC = mysqli_num_rows($result_PC);
-                      $row_PC = mysqli_fetch_assoc($result_PC);
+            WHERE crse_major = 'CC COMS BCN' AND crse_code = '$row_SA['crse_code']'";
+                      $result_P = mysqli_query($conn, $sql_P);
+                      $resultCheck_P = mysqli_num_rows($result_P);
+                      
+$sql_C =  "SELECT crse_code, crse_CO
+            FROM cohort INNER JOIN scheme USING (crse_code)
+            WHERE crse_major = 'CC COMS BCN' AND crse_code = '$row_SA['crse_code']'";
+                      $result_C = mysqli_query($conn, $sql_C);
+                      $resultCheck_C = mysqli_num_rows($result_C);
+                      $row_C = mysqli_fetch_assoc($result_C); 
+      
+      if ($row_SA['crse_year'] >= $est_year && $row_SA['crse_semester'] == $semestre){
+          
+          if($resultCheck_P > 0){ 
+               while($row_P = mysqli_fetch_assoc($result_P)){
+                   
+                   if ()
+               }
+              
+            }
+                      
+          else{          
+          $sql_rec = "UPDATE stdnt_record SET crse_status = 3 WHERE stdnt_number= '$student_id' AND crse_code = '$row_SA['crse_code']' ";}
+   
+             
+                    
+                    
+  }
+  
+                }
+
+ 
 
  $sql_PG =  "SELECT crse_grade
             FROM stdnt_record
