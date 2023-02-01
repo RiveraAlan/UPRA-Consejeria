bf671903861db698eff9011f5bea4d13c1f165fa
RiveraAlan <alan.rivera5@upr.edu>
2023-02-01 11:27:54 -0400

    Final Commit
    This commit was made to have an up to date master before beggining work on this proyect for pushing it live.

--- AdminUPRA/inc/recommend.php
+++ AdminUPRA/inc/recommend.php
@@ -14,9 +14,7 @@ require 'connection.php';
     } else {
         $crse_status = 0; 
     }
-    
-    if($resultCheck > 0){
-        $mydate=getdate(date("U"));
+    $mydate=getdate(date("U"));
         if ($mydate[mon] < 10 && $mydate[mday] < 10) 
         $date = "$mydate[year]-0$mydate[mon]-0$mydate[mday]";
         else if($mydate[mon] < 10)
@@ -26,6 +24,7 @@ require 'connection.php';
         else
         $date = "$mydate[year]-$mydate[mon]-$mydate[mday]";
 
+    if($resultCheck > 0){
     $sql = "UPDATE stdnt_record SET crse_status = $crse_status, date_R = '$date' 
     WHERE stdnt_number = '$stdnt_number' AND crse_code = '$crse_code'";
     // Prepare statement
@@ -36,8 +35,8 @@ require 'connection.php';
     header("Location: ../est_profile.php");
     exit();
     } else {
-        $sql = "INSERT INTO stdnt_record (stdnt_number, crse_code, crse_status) 
-        VALUES ('$stdnt_number','$crse_code', 3)";
+        $sql = "INSERT INTO stdnt_record (stdnt_number, crse_code, crse_status, date_R) 
+        VALUES ('$stdnt_number','$crse_code', 3, '$date')";
     // Prepare statement
     $stmt = $conn->prepare($sql);
     // execute the query
@@ -47,58 +46,4 @@ require 'connection.php';
     exit();
     }
 }
-// elseif(isset($_POST['rec-adi'])){
-//     require '../../private/dbconnect.php';
-//     $stdnt_number = mysqli_real_escape_string($conn, $_POST['stdnt_number']);
-//     $recomendacion = mysqli_real_escape_string($conn, $_POST['rec-adi']);
-
-//     $sql = "SELECT crse_suggestionHUMA, crse_suggestionCISO, crse_suggestionFREE, crse_suggestionDEP FROM counseling_special_details WHERE stdnt_number = '$stdnt_number'";
-//     $result = mysqli_query($conn, $sql);
-//     $resultCheck = mysqli_num_rows($result);
-//     $status = 5;
-
-//     if($resultCheck > 0){
-//         if($recomendacion == 'crse_suggestionHUMA'){
-//             $sql = "UPDATE counseling_special_details SET  crse_suggestionHUMA = $status WHERE stdnt_number = '$stdnt_number'";
-//         }elseif($recomendacion == 'crse_suggestionCISO'){
-//             $sql = "UPDATE counseling_special_details SET  crse_suggestionCISO = $status WHERE stdnt_number = '$stdnt_number'";
-//         }elseif($recomendacion == 'crse_suggestionFREE'){
-//             $sql = "UPDATE counseling_special_details SET  crse_suggestionFREE = $status WHERE stdnt_number = '$stdnt_number'";
-//         }elseif($recomendacion == 'crse_suggestionDEP'){
-//             $sql = "UPDATE counseling_special_details SET  crse_suggestionDEP = $status WHERE stdnt_number = '$stdnt_number'";
-//         }
-//         echo $sql;
-//     // Prepare statement
-//     $stmt = $conn->prepare($sql);
-//     // execute the query
-//     $stmt->execute();
-//     //exit
-//     header("Location: ../est_profile.php");
-//     $stmt->close();
-//     }else{
-//         if($recomendacion == 'crse_suggestionHUMA'){
-//             $stmt = $conn->prepare("INSERT INTO counseling_special_details (stdnt_number, crse_suggestionHUMA) VALUES (?, ?)");
-//         }elseif($recomendacion == 'crse_suggestionCISO'){
-//             $stmt = $conn->prepare("INSERT INTO counseling_special_details (stdnt_number, crse_suggestionCISO) VALUES (?, ?)");
-//         }elseif($recomendacion == 'crse_suggestionFREE'){
-//             $stmt = $conn->prepare("INSERT INTO counseling_special_details (stdnt_number, crse_suggestionFREE) VALUES (?, ?)");
-//         }elseif($recomendacion == 'crse_suggestionDEP'){
-//             $stmt = $conn->prepare("INSERT INTO counseling_special_details (stdnt_number, crse_suggestionDEP) VALUES (?, ?)");
-//         }
-        
-
-//         // Now we tell the script which variable each placeholder actually refers to using the bindParam() method
-//         // First parameter is the placeholder in the statement above - the second parameter is a variable that it should refer to
-//         $stmt->bind_param('si', $stdnt_number, $status);
-//         echo $stdnt_number;
-//         // Execute the query using the data we just defined
-//         // The execute() method returns TRUE if it is successful and FALSE if it is not, allowing you to write your own messages here
-//         if ($stmt->execute()) {
-//             header('Location: ../est_profile.php');
-//         } else {
-//         echo "Unable to create record";
-//         }
-// 	    $stmt->close();
-//     }
-// }
 ?>
